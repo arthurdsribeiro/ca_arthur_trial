a24e5916f2a4e69fd7fd27537a31d6138f6ca814
Arthur Ribeiro <arthur.ribeiro@toptal.com>
2020-06-01 16:39:52 -0300

    Serializes users when retrieving reviews and use a different serializer when creating a review

--- evaluation/serializers.py
+++ evaluation/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from evaluation.models import Review
+from evaluation.models import Review, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('id', 'date')
+
+
+class ReviewWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
--- evaluation/views.py
+++ evaluation/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.permissions import IsAuthenticated
-from evaluation.serializers import ReviewSerializer
+from rest_framework.response import Response
+from evaluation.serializers import ReviewSerializer, ReviewWriteSerializer
 
 
 class ReviewViewSet(mixins.CreateModelMixin,
                     mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
                     mixins.ListModelMixin,
                     viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
@@ -13,3 +13,29 @@ class ReviewViewSet(mixins.CreateModelMixin,
 
     def get_queryset(self):
         return self.request.user.reviews.all()
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def create(self, request, *args, **kwargs):
+        request.data['user'] = request.user.id
+        request.data['ip_address'] = self.get_client_ip(request)
+
+        serializer = ReviewWriteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+
+        read_serializer = ReviewSerializer(serializer.instance)
+        headers = self.get_success_headers(read_serializer.data)
+
+        return Response(
+            ReviewSerializer(serializer.instance).data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
