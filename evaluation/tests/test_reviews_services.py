a30ba4369de64b82e49fac22b2c52cda08452bec
Arthur Ribeiro <arthur.ribeiro@toptal.com>
2020-06-01 16:40:29 -0300

    Adds unit testing for the rest of the CRUD operations

--- evaluation/tests/test_reviews_services.py
+++ evaluation/tests/test_reviews_services.py
@@ -22,7 +22,7 @@ class ReviewsServicesTestCase(TestCase):
         )
 
         # Assign Reviews to users
-        Review.objects.create(
+        self.review1 = Review.objects.create(
             user=self.first_user,
             rating=4,
             title='Test Review 01',
@@ -30,7 +30,7 @@ class ReviewsServicesTestCase(TestCase):
             company='Company 01',
             ip_address='127.0.0.1'
         )
-        Review.objects.create(
+        self.review2 = Review.objects.create(
             user=self.second_user,
             rating=2,
             title='Test Review 02',
@@ -39,19 +39,24 @@ class ReviewsServicesTestCase(TestCase):
             ip_address='127.0.0.1'
         )
 
-        # Retrieves access tokens
+        # Client object to perform requests
         self.client = APIClient()
 
     def test_list_unauthenticated_reviews(self):
+        # Arrange
         expected_response = {
             "detail": "Authentication credentials were not provided."
         }
+
+        # Act
         response = self.client.get(reverse('evaluation:reviews-list'))
 
+        # Assert
         self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEquals(response.data, expected_response)
 
     def test_list_wrong_credentials(self):
+        # Arrange
         expected_response = {
             "detail": 'Given token not valid for any token type',
             "code": "token_not_valid",
@@ -66,6 +71,7 @@ class ReviewsServicesTestCase(TestCase):
 
         self.client.credentials(HTTP_AUTHORIZATION='Bearer wrong_token')
 
+        # Act
         response = self.client.get(
             reverse('evaluation:reviews-list'),
             headers={
@@ -73,6 +79,7 @@ class ReviewsServicesTestCase(TestCase):
             }
         )
 
+        # Assert
         self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEquals(response.data, expected_response)
 
@@ -114,3 +121,136 @@ class ReviewsServicesTestCase(TestCase):
 
         # Assert
         self.assertEquals(response.data, expected_response)
+
+    def test_retrive_review_first_user_success(self):
+        # Arrange
+        serializer = ReviewSerializer(self.review1)
+        self.client.force_authenticate(self.first_user)
+
+        # Act
+        response = self.client.get(
+            reverse('evaluation:reviews-detail', args=[self.review1.id])
+        )
+
+        # Assert
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data, serializer.data)
+
+    def test_retrive_review_second_user_success(self):
+        # Arrange
+        serializer = ReviewSerializer(self.review2)
+        self.client.force_authenticate(self.second_user)
+
+        # Act
+        response = self.client.get(
+            reverse('evaluation:reviews-detail', args=[self.review2.id])
+        )
+
+        # Assert
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data, serializer.data)
+
+    def test_retrive_review_unrelated_to_first_user(self):
+        # Arrange
+        self.client.force_authenticate(self.first_user)
+
+        # Act
+        response = self.client.get(
+            reverse('evaluation:reviews-detail', args=[self.review2.id])
+        )
+
+        # Assert
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_not_available(self):
+        # Arrange
+        self.client.force_authenticate(self.first_user)
+
+        # Act
+        response = self.client.delete(
+            reverse('evaluation:reviews-detail', args=[self.review1.id])
+        )
+
+        # Assert
+        self.assertEquals(
+            response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+    def test_create_review_success(self):
+        # Arrange
+        self.client.force_authenticate(self.first_user)
+        reviews_before_insert = self.first_user.reviews.count()
+
+        new_review = Review()
+        new_review.title = 'Title New'
+        new_review.summary = 'Summary New'
+        new_review.rating = 5
+        new_review.company = 'New Company'
+
+        serializer = ReviewSerializer(new_review)
+
+        # Act
+        response = self.client.post(
+            reverse('evaluation:reviews-list'),
+            serializer.data,
+        )
+
+        # Assert
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEquals(1, reviews_before_insert)
+        self.assertEquals(
+            2, Review.objects.filter(user=self.first_user).count()
+        )
+
+    def test_create_review_success_forwarded_ip(self):
+        # Arrange
+        self.client.force_authenticate(self.first_user)
+        reviews_before_insert = self.first_user.reviews.count()
+
+        new_review = Review()
+        new_review.title = 'Title New'
+        new_review.summary = 'Summary New'
+        new_review.rating = 5
+        new_review.company = 'New Company'
+
+        serializer = ReviewSerializer(new_review)
+
+        # Act
+        response = self.client.post(
+            reverse('evaluation:reviews-list'),
+            serializer.data,
+            HTTP_X_FORWARDED_FOR="8.8.8.8"
+        )
+
+        # Assert
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEquals(1, reviews_before_insert)
+        self.assertEquals(
+            2, Review.objects.filter(user=self.first_user).count()
+        )
+
+    def test_create_view_wrong_rating(self):
+        # Arrange
+        self.client.force_authenticate(self.first_user)
+        reviews_before_insert = self.first_user.reviews.count()
+
+        new_review = Review()
+        new_review.title = 'Title New'
+        new_review.summary = 'Summary New'
+        new_review.rating = 10
+        new_review.company = 'New Company'
+
+        serializer = ReviewSerializer(new_review)
+
+        # Act
+        response = self.client.post(
+            reverse('evaluation:reviews-list'),
+            serializer.data,
+        )
+
+        # Assert
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(1, reviews_before_insert)
+        self.assertEquals(
+            1, Review.objects.filter(user=self.first_user).count()
+        )
